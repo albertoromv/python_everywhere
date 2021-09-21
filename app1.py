73388d9da2c24faae02df04103e90ea16cb8a34f
albertoromv <albertoromv@gmail.com>
2021-09-21 19:12:43 +0000

    corregido abc

--- app1.py
+++ app1.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-	return "<h1>Distant Reading Archive</h1><p>CAMBIADO 4 USANDO GIT This site is a prototype API for distant reading of science fiction novels.</p>"
+	return "<h1>Distant Reading Archive</h1><p>CAMBIADO 321 USANDO GIT This site is a prototype API for distant reading of science fiction novels.</p>"
 
 if __name__ == '__main__':
 	app.run()
